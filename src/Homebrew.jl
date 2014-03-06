1aa710df6e75d346ef650dcdccd467a47094faa1
Isaiah <isaiah.norton@gmail.com>
2014-03-06 14:30:26 -0500

    Update cctools_bundle URL

--- src/Homebrew.jl
+++ src/Homebrew.jl
@@ -9,7 +9,7 @@ const tappath = joinpath(brew_prefix,"Library","Taps","staticfloat-juliadeps")
 
 const BREW_URL = "https://github.com/staticfloat/homebrew.git"
 const BREW_BRANCH = "kegpkg"
-const BOTTLE_SERVER = "http://archive.org/download/julialang/"
+const BOTTLE_SERVER = "http://s3.amazonaws.com/julialang/bin/osx/extras/"
 
 
 function init()
